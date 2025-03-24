84b0841210903dfe8066fa8b5a7f6ff5ee6daf90
tj-bu-706-12 <tj-bu-706-12@DESKTOP-LTSGM2H>
2025-03-24 18:13:57 +0900

    Day11

---
+++ Java/src/codeup/_1093.java
@@ -0,0 +1,20 @@
+package codeup;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class _1093 {
+
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int []array = new int[N];
+		
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array [i] + " ");
+		}
+		
+		sc.close();
+	}
+}
